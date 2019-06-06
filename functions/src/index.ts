b2e294db80f24240454313f2f4e3af96be55ff87
chamikabm <chamika@bitsmedia.com>
2019-06-06 16:40:02 +0800

    Added changes for reading data from the Cloud Filestore.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,20 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+const serviceAccount = require('./../ServiceAccountKey.json');
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+}); // Initialize the admin sdk before it use.
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const getBostonWeather = functions.https.onRequest((request, response) => {
+  admin.firestore().doc('cities-weather/boston-ma-us').get()
+      .then(snapshot => {
+        const data = snapshot.data(); // Javascript object
+        response.send(data); // response object knows how to do the conversion from JS object to JSON strings.
+      })
+      .catch(err => {
+        const errorMessage = `Error occurred. Due to : ${err}`;
+        console.error(errorMessage);
+        response.status(500).send(errorMessage);
+     });
 });
