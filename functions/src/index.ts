88a4312f32e72dcf57a4b29bf0af9c096a66cf03
chamikabm <chamika@bitsmedia.com>
2019-06-07 10:18:50 +0800

    Added changes for sending FCM for on updating the records.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,6 +6,21 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 }); // Initialize the admin sdk before it use.
 
+export const onBostonWeatherUpdate = functions.firestore.document('cities-weather/boston-ma-us')
+.onUpdate(change => {
+    const afterUpdate = change.after.data() || {}; // This give the JS object once the data on the specified doc got changed.
+    const payload = {
+        data: {
+            temp: String(afterUpdate.temp), // It requires to have any other data formats to be in String format for FCM (Firebase Cloud Messaging).
+            conditions: afterUpdate.conditions
+        }
+    };
+
+    return admin.messaging().sendToTopic("weather_boston_ma_ua", payload).catch(err => {
+        console.error(`Error while sending the FCM. Error: ${err}`)
+    });
+});
+
 export const getBostonWeather = functions.https.onRequest((request, response) => {
   admin.firestore().doc('cities-weather/boston-ma-us').get()
       .then(snapshot => {
